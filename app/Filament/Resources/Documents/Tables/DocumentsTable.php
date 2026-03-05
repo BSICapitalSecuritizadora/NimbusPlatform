827e373d2aa9cb1749c04e2b0f328bfbeadb427c
AndersonCavalcanteBSI <8729281+AndersonC96@users.noreply.github.com>
2026-03-05 15:58:38 -0300

    Fix Action import namespace
    Replace incorrect import of Action (Filament\Tables\Actions\Action) with the correct namespace (Filament\Actions\Action) in DocumentsTable.php to prevent class-not-found errors when using Actions in the documents table.

--- app/Filament/Resources/Documents/Tables/DocumentsTable.php
+++ app/Filament/Resources/Documents/Tables/DocumentsTable.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Resources\Documents\Tables;
 
-use Filament\Tables\Actions\Action;
+use Filament\Actions\Action;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
